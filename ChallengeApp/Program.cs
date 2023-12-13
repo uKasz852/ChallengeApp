12a2f3a5217cec45c1fd6f218910dfa9e2065008
ukasz852 <ukasz@UKASZ852>
2023-12-13 21:14:14 +0100

    Zadanie - Dzień 4

--- ChallengeApp/Program.cs
+++ ChallengeApp/Program.cs
@@ -1,1 +1,16 @@
-﻿Console.WriteLine("Hello, World!");
+﻿var name = "Kamil";
+string gender = "Man";
+var age = 17;
+
+if (gender == "Kobieta" && age < 30)
+{
+    Console.WriteLine("Kobieta poniżej 30 lat");
+}
+else if (name == "Ewa" && age == 33)
+{
+    Console.WriteLine("Ewa, lat 33");
+}
+else if (gender != "Woman" && age < 18)
+{
+    Console.WriteLine("Niepełnoletni mężczyzna");
+}

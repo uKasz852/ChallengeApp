9bde89628f9f2e5b55f20593951f0718e993dcee
ukasz852 <ukasz@UKASZ852>
2024-01-15 20:04:43 +0100

    Zadanie - Dzień 9

---
+++ ChallengeApp.Tests/EmployeeTests.cs
@@ -0,0 +1,56 @@
+﻿namespace ChallengeApp.Tests
+{
+    public class EmployeeTests
+    {
+        [Test]
+        public void WhenGetStatisticsCalled_ShouldReturnCorrectMax()
+        {
+            //arrange
+            var employee = new Employee("Łukasz", "Wysocki");
+            employee.AddGrade(8);
+            employee.AddGrade(4);
+            employee.AddGrade(9);
+
+
+            //act
+            var statistics = employee.GetStatistics();
+
+            //assert
+            Assert.AreEqual(9, statistics.Max);
+        }
+
+        [Test]
+        public void WhenGetStatisticsCalled_ShouldReturnCorrectMin()
+        {
+            //arrange
+            var employee = new Employee("Łukasz", "Wysocki");
+            employee.AddGrade(8);
+            employee.AddGrade(4);
+            employee.AddGrade(9);
+
+
+            //act
+            var statistics = employee.GetStatistics();
+
+            //assert
+            Assert.AreEqual(4, statistics.Min);
+        }
+
+        [Test]
+        public void WhenGetStatisticsCalled_ShouldReturnCorrectAvarage()
+        {
+            //arrange
+            var employee = new Employee("Łukasz", "Wysocki");
+            employee.AddGrade(8);
+            employee.AddGrade(4);
+            employee.AddGrade(9);
+
+
+            //act
+            var statistics = employee.GetStatistics();
+
+            //assert
+            Assert.AreEqual(7, statistics.Average);
+        }
+    }
+}
